3afb3303f5d3e70e7c7fd1ed69baa339795090e5
Chamila Maddumage <chamila.maddumage-sc@airliquide.com>
2020-03-10 15:20:06 +0530

    initial commit

---
+++ src/app/models/device.ts
@@ -0,0 +1,17 @@
+export class Device{
+   
+    device_id :number
+    device_type: string
+    model:string
+    serial_no:string
+    quantity:number
+    device_owner:string
+    windows_key:string
+    airliquide_id_no:string
+    bl_key:string
+    working_condition:string
+    floor:number
+    warranty:number
+    service_history:string
+
+} 
